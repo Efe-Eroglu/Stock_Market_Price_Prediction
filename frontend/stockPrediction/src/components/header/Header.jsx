4994db117bb13108ded21a253c3382e28c5a90c5
Efe <eferoglu1967@gmail.com>
2023-12-13 16:03:27 +0300

    Main Page

--- frontend/stockPrediction/src/components/header/Header.jsx
+++ frontend/stockPrediction/src/components/header/Header.jsx
@@ -6,24 +6,24 @@ const Header = () => {
   return (
     <>
       <header className="header w-screen h-16 flex items-center justify-between pl-0 pt-0 ml-0 mb-10">
-        <div className="text-white ml-4 text-2xl">Stock Prediction</div>
+        <div className="text-white ml-4 text-3xl">Stock Prediction</div>
         <div className="flex space-x-4 mr-4">
-          <Link to="/" className="text-white">
-            <span className="a">Ana Sayfa</span>
+          <Link to="/" className="text-white hover:text-white active:text-white focus:text-white" >
+            <span className="linkler">Ana Sayfa</span>
           </Link>
-          <Link to="/share" className="text-white">
-          <span className="a">Tablolar</span>
+          <Link to="/share" className="text-white hover:text-white active:text-white focus:text-white">
+          <span className="linkler">Tablolar</span>
           </Link>
-          <Link to="/about" className="text-white">
-          <span className="a">Hakkımızda</span>
+          <Link to="/about" className="text-white hover:text-white active:text-white focus:text-white">
+          <span className="linkler">Hakkımızda</span>
+          </Link> 
+          <Link to="/team" className="text-white hover:text-white active:text-white focus:text-white">
+          <span className="linkler">Ekibimiz</span>
           </Link>
-          <Link to="/team" className="text-white">
-          <span className="a">Ekibimiz</span>
+          <Link to="/contact" className="text-white hover:text-white active:text-white focus:text-white">
+          <span className="linkler">İletişim</span>
           </Link>
-          <Link to="/contact" className="text-white">
-          <span className="a">İletişim</span>
-          </Link>
-          <Link to="/login" className="text-white flex items-center">
+          <Link to="/login" className="text-white flex items-center hover:text-white active:text-white focus:text-white">
             <FaRegUser className="text-lg mr-2 ml-2" />
           </Link>
         </div>
--- frontend/stockPrediction/src/pages/AnaSayfa/AnaSayfa.jsx
+++ frontend/stockPrediction/src/pages/AnaSayfa/AnaSayfa.jsx
@@ -1,15 +1,72 @@
-import Header from "../../components/header/Header"
-import "./anaSayfa.css"
+import Header from "../../components/header/Header";
+import "./anaSayfa.css";
 
 const AnaSayfa = () => {
   return (
     <>
-    
-    <Header/>
-    <h1 className="mt-10">Ana Sayfa</h1>    
-    </>
-    
-  )
-}
+      <Header />
 
-export default AnaSayfa
+      <div className="block">
+        <img src="mainPageImages/hero-bg.png" className="heroBg" alt="" />
+      </div>
+      <section className="slider_section">
+        <div
+          id="customCarousel1"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner z-5">
+            <div className="carousel-item active">
+              <div className="">
+                <div className="row">
+                  <div className="col-md-6 ">
+                    <div className="detail-box">
+                      <h1>
+                        HİPER <br />
+                        PARAMETRELER
+                      </h1>
+                      <p>
+                        Bist 30 hisseleri üzerinde State of Art modelinin en
+                        doğru parametrelerle eğitilmiş modelini hemen incele!
+                      </p>
+                      <div className="btn-box">
+                        <a href="" className="btn1">
+                          Hemen İncele
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="img-box">
+                      <img
+                        src="mainPageImages/banner-img1.png"
+                        id="resim"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="slider-button">
+          <ol className="carousel-indicators">
+            <li
+              data-target="#customCarousel1"
+              data-slide-to="0"
+              className="active"
+            ></li>
+            <li data-target="#customCarousel1" data-slide-to="1"></li>
+            <li data-target="#customCarousel1" data-slide-to="2"></li>
+          </ol>
+        </div>
+      
+      </section>
+     
+    </>
+  );
+};
+
+export default AnaSayfa;
