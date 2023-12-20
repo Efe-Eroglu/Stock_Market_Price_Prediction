9ed9c5478217f79565540a3b7556fffac4d7baef
Efe <eferoglu1967@gmail.com>
2023-12-21 00:11:56 +0300

    Get single share

--- backend/routes/share.js
+++ backend/routes/share.js
@@ -2,20 +2,35 @@ const express = require("express");
 const router = express.Router();
 const ShareSchema = require("../models/share_schema.js");
 
-
-
+//Tüm Hisseleri Getir
 router.get("/", async (req, res) => {
   try {
     const hisse = await ShareSchema.find();
 
     res.status(200).json(hisse);
-
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "Server Error" });
   }
 });
 
-
+router.get("/:hisseID", async (req, res) => {
+  try {
+    const hisseID = req.params.hisseID;
+    try {
+      const hisse = await ShareSchema.findById(hisseID);
+      if (!hisse) {
+        return res.status(404).json({ error: "Share not found" });
+      }
+      res.status(200).json(hisse);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ error: "Share not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
 
 module.exports = router;
